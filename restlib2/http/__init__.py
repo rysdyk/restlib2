74d5d35d7aae965934cab547cea34c51b3ff9794
srysdyk <srysdyk@iu.edu>
2020-01-06 16:35:39 -0500

    updates for python3

--- restlib2/http/__init__.py
+++ restlib2/http/__init__.py
@@ -1,20 +1,21 @@
-from django.core.handlers.wsgi import STATUS_CODE_TEXT
+# from django.core.handlers.wsgi import STATUS_CODE_TEXT
+import http
 from restlib2.structures import AttrDict
 
-STATUS_CODE_TEXT.setdefault(422, 'UNPROCESSABLE ENTITY')
+http.client.responses.setdefault(422, 'UNPROCESSABLE ENTITY')
 
 # http://tools.ietf.org/html/rfc6585
-STATUS_CODE_TEXT.setdefault(428, 'PRECONDITION REQUIRED')
-STATUS_CODE_TEXT.setdefault(429, 'TOO MANY REQUESTS')
-STATUS_CODE_TEXT.setdefault(431, 'REQUEST HEADER FIELDS TOO LARGE')
-STATUS_CODE_TEXT.setdefault(511, 'NETWORK AUTHENTICATION REQUIRED')
+http.client.responses.setdefault(428, 'PRECONDITION REQUIRED')
+http.client.responses.setdefault(429, 'TOO MANY REQUESTS')
+http.client.responses.setdefault(431, 'REQUEST HEADER FIELDS TOO LARGE')
+http.client.responses.setdefault(511, 'NETWORK AUTHENTICATION REQUIRED')
 
 # http://tools.ietf.org/html/draft-tbray-http-legally-restricted-status-02
-STATUS_CODE_TEXT.setdefault(451, 'UNAVAILABLE FOR LEGAL REASONS')
+http.client.responses.setdefault(451, 'UNAVAILABLE FOR LEGAL REASONS')
 
 # Invert dict for reference by name
 codes = AttrDict('HTTP Status Codes',
-    [(y, x) for x, y in STATUS_CODE_TEXT.items()])
+    [(y, x) for x, y in http.client.responses.items()])
 
 
 # PATCH Method introduced - http://tools.ietf.org/html/rfc5789
